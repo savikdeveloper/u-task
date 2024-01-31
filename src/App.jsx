98ff5af420dfaf288fda185c572e6a33e95c8211
savikdeveloper <savikdeveloper@gmail.com>
2024-01-31 14:12:14 +0500

    Added rating tags and fixed errors

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,46 @@ import { useState, useEffect, useMemo } from 'react';
 
 function App() {
   const [rowData, setRowData] = useState([]);
-  
-  // Column Definitions: Defines & controls grid columns.
+
   const [colDefs, setColDefs] = useState([
     { field: "id", sortable: true, filter: true },
     { field: "title", sortable: true, filter: true },
-    { field: "tags", headerName: "Tags" },
+    {
+      field: "tags",
+      headerName: "Tags",
+      valueFormatter: (params) => {
+        const tagNames = params.value.map(tag => tag.name).join(", ");
+        return tagNames;
+      },
+    },
     { field: "difficultyTitle", sortable: true, filter: true },
-    { field: "likesCount", headerName: "Rating", sortable: true, filter: true},
+    {
+      field: "likesCount",
+      headerName: "Rating",
+      sortable: true,
+      filter: true,
+      valueGetter: (params) => {
+        const likesCount = params.data.likesCount || 0;
+        const dislikeCount = params.data.dislikeCount || 0;
+        return `Like ${likesCount} / Dislike ${dislikeCount}`;
+      },
+    },
     { field: "solved", sortable: true, filter: true },
   ]);
 
   const defaultColDef = useMemo(() => ({
     sortable: true,
-    filter: true
+    filter: true,
   }), [])
 
   useEffect(() => {
-    fetch('https://kep.uz/api/problems') // Fetch data from server
-      .then(result => result.json()) // Convert to JSON
-      .then(rowData => setRowData(rowData.data)); // Update state of `rowData`
+    fetch('https://kep.uz/api/problems')
+      .then(result => result.json())
+      .then(rowData => setRowData(rowData.data));
   }, [])
 
   return (
-    <div className="ag-theme-quartz" style={{ height: 500 , width: 1250}}>
+    <div className="ag-theme-quartz" style={{height: 500}}>
       <AgGridReact 
         rowData={rowData} 
         columnDefs={colDefs}
