3c735fccca86c432bc49df37cc67b5399767eff3
savikdeveloper <savikdeveloper@gmail.com>
2024-01-31 12:21:52 +0500

    added filter and sortable

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,25 @@ import './App.css'
 import { AgGridReact } from 'ag-grid-react'; // React Grid Logic
 import "ag-grid-community/styles/ag-grid.css"; // Core CSS
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Theme
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 
 function App() {
   const [rowData, setRowData] = useState([]);
   
   // Column Definitions: Defines & controls grid columns.
   const [colDefs, setColDefs] = useState([
-    { field: "id" },
-    { field: "title" },
-    { field: "tags" },
-    { field: "difficultyTitle" },
-    { field: "likesCount"},
-    { field: "solved" },
+    { field: "id", sortable: true, filter: true },
+    { field: "title", sortable: true, filter: true },
+    { field: "tags", headerName: "Tags" },
+    { field: "difficultyTitle", sortable: true, filter: true },
+    { field: "likesCount", headerName: "Rating", sortable: true, filter: true},
+    { field: "solved", sortable: true, filter: true },
   ]);
+
+  const defaultColDef = useMemo(() => ({
+    sortable: true,
+    filter: true
+  }), [])
 
   useEffect(() => {
     fetch('https://kep.uz/api/problems') // Fetch data from server
@@ -24,8 +29,14 @@ function App() {
   }, [])
 
   return (
-    <div className="ag-theme-quartz" style={{ height: 500 }}>
-      <AgGridReact rowData={rowData} columnDefs={colDefs} />
+    <div className="ag-theme-quartz" style={{ height: 500 , width: 1250}}>
+      <AgGridReact 
+        rowData={rowData} 
+        columnDefs={colDefs}
+        defaultColDef={defaultColDef}
+        rowSelection='multiple'
+        animateRows={true}
+      />
     </div>
   )
 }
